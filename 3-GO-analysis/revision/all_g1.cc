672c4b5f48d2872a11f0dee444070764d41d241d
Dennis Lavrov <dlavrov@iastate.edu>
2022-02-18 11:57:15 -0600

    Added data for G1 and G2 for the GO analysis

---
+++ 3-GO-analysis/revision/all_g1.cc
@@ -0,0 +1,26 @@
+>DM_Dictyodendrilla_dendyi
++rnl +cox1 +nad1 +cox2 +atp8 +cob +nad6 +nad3 +nad4L +atp6 +cox3 +nad4 +trnM(cau) +nad2 +trnW(uca) +nad5 +rns +atp9 
+>DM_Igernella_notabilis 
++rnl +cox1 +nad1 +cox2 +atp8 +cob +nad6 +nad3 +nad4L +atp6 +cox3 +nad4 +trnM(cau) +nad2 +trnW(uca) +nad5 +rns +atp9 
+>DM_Carteriospongia_foliascens 
++rnl +atp9 +cox1 +nad1 +cob +nad4 +nad6 +nad3 +nad4L +cox2 +atp8 +atp6 +cox3 +trnW(uca) +trnM(cau) +nad2 +nad5 +rns 
+>DM_Cacospongia_mycofijiensis
++rnl +atp9 +cox1 +nad1 +cob +nad4 +nad6 +nad3 +nad4L +cox2 +atp8 +atp6 +cox3 +trnW(uca) +trnM(cau) +nad2 +nad5 +rns 
+>DM_Dysidea_etheria      
++rnl +atp9 +cox1 +nad1 +cob +nad4 +nad6 +miss +nad4L +cox2 +trnW(uca) +atp8 +atp6 +cox3 +nad2 +nad5 +trnM(cau) +rns 
+>DM_Darwinella_gardiner  
++rnl +atp9 +cox1 +nad1 +cob +nad4 +nad6 +nad3 +nad4L +cox2 +atp8 +atp6 +cox3 +trnW(uca) +trnM(cau) +nad2 +nad5 +rns 
+>DM_Hippospongia_lachne 
++rnl +atp9 +cox1 +nad1 +cob +nad4 +nad6 +nad3 +nad4L +cox2 +atp8 +atp6 +cox3 +trnW(uca) +trnM(cau) +nad2 +nad5 +rns 
+>DM_Ircinia_strobilina 
++rnl +atp9 +cox1 +nad1 +cob +nad4 +nad6 +nad3 +nad4L +cox2 +atp8 +atp6 +cox3 +trnW(uca) +trnM(cau) +nad2 +nad5 +rns 
+>DM_Ircinia_sp_LCJ01
++rnl +atp9 +cox1 +nad1 +cob +nad4 +nad6 +nad3 +nad4L +cox2 +atp8 +atp6 +cox3 +trnW +trnM +nad2 +nad5 +rns 
+>DM_Ircinia_sp_LCJ03
++rnl +atp9 +cox1 +nad1 +cob +nad4 +nad6 +nad3 +nad4L +cox2 +atp8 +atp6 +cox3 +trnW +trnM +nad2 +nad5 +rns 
+>DM_Hyattella_sinuosa_LCJ04
++rnl +atp9 +cox1 +nad1 +cob +nad4 +nad6 +nad3 +nad4L +cox2 +atp8 +atp6 +cox3 +trnW(uca) +trnM(cau) +nad2 +nad5 +rns 
+>DM_Pleraplysilla_spinifera
++rnl +atp9 +cox1 +nad1 +cob +nad4 +nad6 +nad3 +nad4L +cox2 +atp8 +atp6 +cox3 +nad2 +nad5 +rns
+>DM_Vaceletia_sp.       
++rnl +atp9 +cox1 +nad1 +cob +nad4 +nad6 +nad3 +nad4L +cox2 +atp8 +atp6 +cox3 +trnW(uca) +trnM(cau) +nad2 +nad5 +rns 
